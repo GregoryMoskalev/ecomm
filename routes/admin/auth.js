625a69507afefe09220efcf04a8e4b810258b0d9
Onberfrest <62020954+Onberfrest@users.noreply.github.com>
2020-07-02 19:13:38 +0300

    validation around sign in

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { validationResult } = require('express-validator');
+const { check, validationResult } = require('express-validator');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
@@ -39,23 +39,43 @@ router.get('/signin', (req, res) => {
 	res.send(signinTemplate());
 });
 
-router.post('/signin', async (req, res) => {
-	const { email, password } = req.body;
+router.post(
+	'/signin',
+	[
+		check('email')
+			.trim()
+			.normalizeEmail()
+			.isEmail()
+			.withMessage('Must provide a valid email')
+			.custom(async (email) => {
+				const user = await usersRepo.getOneBy({ email });
+				if (!user) {
+					throw new Error('Email not found');
+				}
+			}),
+		check('password').trim()
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		console.log(errors);
 
-	const user = await usersRepo.getOneBy({ email });
+		const { email, password } = req.body;
 
-	if (!user) {
-		return res.send('Email not found');
+		const user = await usersRepo.getOneBy({ email });
+
+		// if (!user) {
+		// 	return res.send('Email not found');
+		// }
+
+		const validPassword = await usersRepo.comparePasswords(user.password, password);
+		if (!validPassword) {
+			return res.send('Invalid password');
+		}
+
+		req.session.userId = user.id;
+
+		res.send('You are signed in!');
 	}
-
-	const validPassword = await usersRepo.comparePasswords(user.password, password);
-	if (!validPassword) {
-		return res.send('Invalid password');
-	}
-
-	req.session.userId = user.id;
-
-	res.send('You are signed in!');
-});
+);
 
 module.exports = router;

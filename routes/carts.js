f874488e05db9adbc891a7df6a0f5eca609a64ab
Onberfrest <62020954+Onberfrest@users.noreply.github.com>
2020-07-05 18:31:30 +0300

    creating a cart

--- routes/carts.js
+++ routes/carts.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const cartsRepo = require('../repositories/carts');
 
 const router = express.Router();
 
 //receive a post request to add an item to a cart
-router.post('/cart/products', (req, res) => {
-	console.log(req.body.productId);
+router.post('/cart/products', async (req, res) => {
+	let cart;
+	if (!req.session.cartId) {
+		cart = await cartsRepo.create({ items: [] });
+		req.session.cartId = cart.id;
+	} else {
+		cart = await cartsRepo.getOne(req.session.cartId);
+	}
+	console.log(cart);
 
 	res.send('Product added');
 });
